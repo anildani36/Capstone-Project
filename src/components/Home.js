020f241c54143938fb5dc0ad042d6d9999d6d1f3
Shivam Sikdar <sikdarshivam@gmail.com>
2021-11-29 00:38:44 +0530

    basic changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Home() {
     return (
         <div>
-<<<<<<< HEAD
-=======
-            <h1>HOME COMPONENT</h1>
->>>>>>> 40b7764b4e8f1a223e8e1d1526d203e5c57afac1
-        </div>
+
+
+            <h2>SWEETS, CHOCOLTES & CAKES</h2>
+
+ </div>
     )
 }
 
