6bda74cc47f2a5145718d4343aa2ab74df7224c6
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-01 13:41:43 +0530

    created a services page

---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className="services">
+
+
+            <h1> Our Services </h1>
+
+ </div>
+    )
+}
+
+export default Services
