7eb2f78c604c23f87f9c9b288bfefade8e41ace4
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-05 23:18:00 +0530

    Footer created for website

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import "../css/Footer.css"
+
+function Footer() {
+    return (
+        <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col">
+            <h5>DRACO CAPSTONE PRO</h5>
+            <div className="list-unstyled">
+              <li>7980123346</li>
+              <li>KOLKATA, INDIA</li>
+              <li>123 KGP Street</li>
+            </div>
+          </div>
+          {/* Column2 */}
+          <div className="col">
+            <h5>ABOUT</h5>
+            <ui className="list-unstyled">
+                
+              <li>SWEETS</li>
+              <li>CHOCOLATES</li>
+              <li>CAKES</li>
+              
+            </ui>
+          </div>
+          {/* Column3 */}
+          <div className="col">
+            <h5>CONTACT</h5>
+            <ui className="list-unstyled">
+                
+              <li>FACEBOOK</li>
+              <li>INSTAGRAM</li>
+              <li>EMAIL</li>
+              
+            </ui>
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} DRACO SOAL | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Footer
