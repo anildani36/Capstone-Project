81b09a8c6b7ac20cdd784a285aef4b7df6387ace
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-05 23:20:11 +0530

    Home advanced created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react'
+import {Link} from 'react-router-dom';
+import BannerImage from '../asserts/cake11.jpg';
+import '../css/Home.css';
+
 
 function Home() {
     return (
-        <div>
-
-
-            <h2>SWEETS, CHOCOLTES & CAKES</h2>
+        <div className="home" style={{backgroundImage: `url(${BannerImage})` }}>
+            <div className="headerContainer">
+            <h1>Sweet Basket</h1><br></br>
+            <p>Hungry? Why Wait? Grab A Sweet Treat!!!</p>
+            <Link to="/Services">
+                <Button>Order Now</Button>
+            </Link>
+            </div>
 
  </div>
     )
