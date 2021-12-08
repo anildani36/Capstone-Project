bfae620b609161e0288f1be89a37f01417649f3c
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-08 22:19:13 +0530

    Services provide with cards are done

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Card(props) {
+    return (
+        <div>
+             <div style={{width:'320px',margin:'20px'}}>
+                 <div className="card" >
+  <img src={props.image} className="card-img-top" alt={props.image}/>
+  <div className="card-body">
+    <h5 className="card-title">{props.name}</h5>
+    <p className="card-text">{props.description}</p>
+    <NavLink to="" className="btn btn-primary">Lets Go</NavLink>
+  </div>
+</div>
+                 </div>
+            
+        </div>
+    )
+}
+
+export default Card
+
+
+
---
+++ src/helper/SerData.js
@@ -0,0 +1,39 @@
+import Cakess from '../asserts/cake16.jpg';
+import Chocolate from '../asserts/choco.jpg';
+import Sweet from '../asserts/swe.jpg';
+import Candy from '../asserts/can.jpg';
+import Dessert from '../asserts/pud.jpg';
+import Dryfruit from '../asserts/dryfruits.jpg';
+
+export const SerData =[
+    {
+        image: Cakess,
+        name:"Cakes",
+        description:"If there's a cake in front of you, then you shouldn't look any further for joy! "
+    },
+    {
+        image: Chocolate,
+        name:"Chocolates",
+        description:"Life is a box of chocolates. You never know what you're gonna get. "
+    },
+    {
+        image: Sweet,
+        name:"Sweets",
+        description:"No need for an occasion to eat sweets. "
+    },
+    {
+        image: Candy,
+        name:"Candies",
+        description:"They come in a variety of colours, hard outside and yumminess inside. "
+    },
+    {
+        image: Dessert,
+        name:"Desserts",
+        description:" I want to have a good body but not as much as I want dessert."
+    },
+    {
+        image: Dryfruit,
+        name:"Dried Fruits",
+        description:"Eat dried fruits and be healthy"
+    }
+]
