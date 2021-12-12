423ef281c469dfb9bfedddd65d33264cd9e9a623
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-13 01:52:21 +0530

    Home Setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,19 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import BannerImage from '../asserts/tao1.jpg';
 import '../css/Home.css';
+import {Carousel} from 'react-bootstrap';
+import Dan from '../asserts/ban1.jpg';
+import Dans from '../asserts/ban6.jpg';
+import Danss from '../asserts/ban4.jpg';
+import Dax from '../asserts/food21.jpg';
+
+
+
 
 
 function Home() {
     return (
+        <div className='home1'>
         <div className="home" style={{backgroundImage: `url(${BannerImage})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
@@ -20,8 +29,55 @@ function Home() {
                 <Button>Order Now</Button>
             </Link>
             </div>
-
+            <br></br>
+        
  </div>
+ <div className='someone'  style={{backgroundImage: `url(${Dax})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width:'98.92vw',
+        height:'50vh' }}>
+ <div className='somthing'>
+            <Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Dans}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Dan}
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Danss}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+     
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
+</div>
+            
+ </div>
+ 
     )
 }
 
