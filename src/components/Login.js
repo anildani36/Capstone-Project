01ceb354fadda75a71701ecb21ec9aab9721eba0
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-12 13:45:57 +0530

    login changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../css/Login.css'
-import BannerImage from '../asserts/cake11.jpg';
+import BannerImage from '../asserts/odin1.jpg';
 
 
 function Login() {
