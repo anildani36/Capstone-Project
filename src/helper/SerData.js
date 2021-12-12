0002f9819074e9574b5bd1cf309f4362d5195055
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-12 13:50:42 +0530

    Adding more data and modifiying it

--- src/helper/SerData.js
+++ src/helper/SerData.js
@@ -1,39 +1,114 @@
-import Cakess from '../asserts/cake16.jpg';
-import Chocolate from '../asserts/choco.jpg';
-import Sweet from '../asserts/swe.jpg';
-import Candy from '../asserts/can.jpg';
-import Dessert from '../asserts/pud.jpg';
-import Dryfruit from '../asserts/dryfruits.jpg';
+import Cakess from '../asserts/cake201.jpg';
+import Chocolate from '../asserts/choco1.jpg';
+import Sweet from '../asserts/swe1.jpg';
+import Candy from '../asserts/can1.jpg';
+import Dessert from '../asserts/pud1.jpg';
+import Dryfruit from '../asserts/dried.jpg';
+import White from '../asserts/whitechoco1.jpg';
+import Almond from '../asserts/alam1.jpg';
+import Cashew from '../asserts/kaju1.jpg';
+import Lolipop from '../asserts/can1.jpg';
+import Sweetto from '../asserts/can1.jpg';
+// import Puding from '../asserts/can1.jpg';
+
+
+
 
 export const SerData =[
     {
+        _id: "1",
         image: Cakess,
-        name:"Cakes",
+        names:"Red Velvet Cakes",
+        category: "cake",
+        price: "750",
         description:"If there's a cake in front of you, then you shouldn't look any further for joy! "
     },
     {
+        _id: "2",
         image: Chocolate,
-        name:"Chocolates",
+        names:"Dark Chocolate",
+        category: "chocolate",
+        price: "550",
         description:"Life is a box of chocolates. You never know what you're gonna get. "
     },
     {
+        _id: "3",
         image: Sweet,
-        name:"Sweets",
+        names:"Barfies",
+        category: "cake",
+        price: "403",
         description:"No need for an occasion to eat sweets. So don't wait just have a blast! "
     },
     {
+        _id: "4",
         image: Candy,
-        name:"Candies",
+        names:"Candies",
+        category: "sweet",
+        price: "330",
         description:"They come in a variety of colours, hard outside and yumminess inside. "
     },
     {
+        _id: "5",
         image: Dessert,
-        name:"Desserts",
+        names:"Desserts",
+        category: "dess",
+        price: "100",
         description:" I want to have a good body but not as much as I want dessert."
     },
     {
+        _id: "6",
         image: Dryfruit,
-        name:"Dried Fruits",
+        names:"Dried Fruits",
+        category: "dried",
+        price: " 150",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    },
+    {
+        _id: "7",
+        image: Almond,
+        names:"Almonds",
+        category: "dried",
+        price: " 700",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    }, 
+    {
+        _id: "8",
+        image: Cashew,
+        names:"Cashew Nuts",
+        category: "dried",
+        price: " 230",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    },
+     {
+        _id: "9",
+        image: Sweetto,
+        names:"Milk Made Sweets",
+        category: "sweet",
+        price: " 100",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    },
+     {
+        _id: "10",
+        image: Dryfruit,
+        names:"Dried Fruits",
+        category: "sweet",
+        price: " 70",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    },
+     {
+        _id: "11",
+        image: White,
+        names:"White Chocolate",
+        category: "chocolate",
+        price: " 500",
+        description:"Eat dried fruits and be healthy. You will find everything here"
+    },
+     {
+        _id: "12",
+        image: Lolipop,
+        names:"Dried Fruits",
+        category: "cand",
+        price: "400",
         description:"Eat dried fruits and be healthy. You will find everything here"
     }
 ]
