1ba783f8e65b9851a886cd4f647e789f7e201f83
Dharani <65335017+dharani1303@users.noreply.github.com>
2021-12-13 17:08:47 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ function About() {
             <div className="info">
                 <Card>
                     <Card.Title>What is Project About?</Card.Title>
-                    <Card.Body>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Card.Body>
+                    <Card.Body>Sweet Basket is a online ordering platform where everyone can order cakes, chocolates,cookies etc.. from various shops and home bakers.</Card.Body>
                 </Card>
             </div>
             <div>
