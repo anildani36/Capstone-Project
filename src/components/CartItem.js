c2afd458aae42cbc2cc84f8f8838eb138bc7a411
Shivam Sikdar <sikdarshivam@gmail.com>
2021-12-14 02:18:57 +0530

    CartIteam is updated with css

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,18 +1,19 @@
 import { useContext } from "react";
 import "../css/CartItem.css";
 import CartContext from "../context/cart/CartContext";
-// import formatCurrency from "format-currency";
+import formatCurrency from "format-currency";
 
 const CartItem = ({ item }) => {
   const { removeItem } = useContext(CartContext);
+  let opts = { format: "%s%v", symbol: "Rs " };
   
   return (
     <li className='CartItem__item'>
-      <img src={item.image} alt='' />
+      <img src={item.image} alt='hfhf' />
       <div>
-        {item.names} {item.price}
+        {item.names} {formatCurrency(`${item.price}`, opts)}
       </div>
-      <button className='CartItem__button' onClick={() => removeItem(item._id)}>
+      <button className='CartItem__button' style={{color: 'rgb(64,0,0)'}} onClick={() => removeItem(item._id)}>
         Remove
       </button>
     </li>
